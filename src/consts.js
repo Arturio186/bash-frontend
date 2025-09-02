3058c2e4caf5b7f7e20d24afc76548fe63fbf740
Arturio186 <bain.romanoff@mail.ru>
2025-09-02 18:27:24 +0500

    changes

--- src/consts.js
+++ src/consts.js
@@ -4,5 +4,5 @@ export const paths = {
   DASHBOARD: '/dashboard'
 }
 
-export const backendUrl = 'http://localhost:5000/api';
-export const socketUrl = 'http://localhost:5000';
+export const backendUrl = 'https://bashbtctest.ru/api';
+export const socketUrl = 'http://bashbtctest.ru';
